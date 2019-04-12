fb4ee9cfd9dfce9afee23bf3d9eae4025d7cbdf7
Edwin Zapata <Edwin Zapata@LAPTOP-MGQCJAO4>
2019-04-12 14:39:57 -0500

    Api Voting Events Controller

---
+++ Democracy.Web/Controllers/API/VotingEventsController.cs
@@ -0,0 +1,23 @@
+﻿namespace Democracy.Web.Controllers.API
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class VotingEventsController : Controller
+    {
+        private readonly IVotingEventRepository votingEventRepository;
+
+        public VotingEventsController(IVotingEventRepository votingEventRepository)
+        {
+            this.votingEventRepository = votingEventRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetVotingEvents()
+        {
+            return this.Ok(this.votingEventRepository.GetAll());
+        }
+    }
+
+}
